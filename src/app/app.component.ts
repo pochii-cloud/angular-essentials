3363001a13258813c4e66bb169169ad235c246a9
JohnMwaniki-TheJitu <john.mwaniki@griffinglobaltech.com>
2024-02-29 16:51:01 +0300

    feat:added signal component into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { QuillComponent } from "./components/quill/quill.component";
 import { ParentComponent } from "./components/parent/parent/parent.component";
 import { ChildComponent } from "./components/child/child/child.component";
+import { SignalscomponentComponent } from "./components/signals/signalscomponent/signalscomponent.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, QuillComponent, ParentComponent, ChildComponent]
+    imports: [RouterOutlet, QuillComponent, ParentComponent, ChildComponent, SignalscomponentComponent]
 })
 export class AppComponent {
   title = 'editor-project';
